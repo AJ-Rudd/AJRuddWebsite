aeab92fbcee9839e20ba2a1a9f6fccb3f65cb386
AJ-Rudd <79539228+AJ-Rudd@users.noreply.github.com>
2021-04-30 14:44:57 -0400

    Add files via upload

---
+++ info.js
@@ -0,0 +1,3 @@
+{"info": [{"firstname":"David", "lastname":"Rudd", "preferredname":"AJ", "city":"Bethesda", "state":"MD"}],
+  "professional":[{"areaofstudy":"HCI/AR/VR/Nueroscience/Cognition", "codeLanguages":"Javascript/HTML/CSS/Python",
+  "programs":"Photoshop/Illustrator/Unity"}]}
