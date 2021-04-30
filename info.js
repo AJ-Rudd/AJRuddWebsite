cb8fb574a18a305e615a2cfc6032f1a46544fa62
AJ-Rudd <79539228+AJ-Rudd@users.noreply.github.com>
2021-04-30 14:45:34 -0400

    Delete info.js

--- info.js
+++
@@ -1,3 +0,0 @@
-{"info": [{"firstname":"David", "lastname":"Rudd", "preferredname":"AJ", "city":"Bethesda", "state":"MD"}],
-  "professional":[{"areaofstudy":"HCI/AR/VR/Nueroscience/Cognition", "codeLanguages":"Javascript/HTML/CSS/Python",
-  "programs":"Photoshop/Illustrator/Unity"}]}
